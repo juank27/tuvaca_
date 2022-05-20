a09d1ba0d281782503014b857a48ce90eaac973d
NayibeCasas <nayibepelaez03@gmail.com>
2022-05-19 21:00:25 -0500

    modal de publicaciones

--- JS/index.js
+++ JS/index.js
@@ -547,3 +547,70 @@ precio.addEventListener("click", function () {
     precios.style.visibility="visible"
     precios.style.height= "auto";
 })
+/*****************************modal busqueda*******************************/
+let publicaciones = document.querySelectorAll(".publicaciones")[0];
+let modalPublicacion = document.querySelectorAll(".modal_container_P")[0];
+let cerrarPublicacion = document.querySelectorAll(".cerrar_modal_P")[0];
+let modalPublicacion2 = document.querySelectorAll(".modal_P")[0];
+publicaciones.addEventListener("click", function () {
+
+    modalPublicacion.style.opacity = "1";
+    modalPublicacion.style.visibility = "visible";
+    modalPublicacion2.classList.toggle("modal_cerrado_P")
+})
+cerrarPublicacion.addEventListener("click", function () {
+    modalPublicacion2.classList.toggle("modal_cerrado_P");
+    setTimeout(function () {
+        modalPublicacion.style.opacity = "0";
+        modalPublicacion.style.visibility = "hidden";
+    }, 900)
+})
+let C1 = document.querySelectorAll(".C")[0];
+let E1 = document.querySelectorAll(".E")[0];
+let P1 = document.querySelectorAll(".P")[0];
+let B1 = document.querySelectorAll(".B")[0];
+let C = document.querySelectorAll(".modal_crear")[0];
+let E = document.querySelectorAll(".modal_editar")[0];
+let P = document.querySelectorAll(".modal_publicar")[0];
+let B = document.querySelectorAll(".modal_borrar")[0];
+
+C1.addEventListener("click", function () {
+    C.style.visibility="visible"
+    C.style.height= "auto";
+    E.style.visibility = "hidden";
+    E.style.height= "0px";
+    P.style.visibility = "hidden";
+    P.style.height= "0px";
+    B.style.visibility = "hidden";
+    B.style.height= "0px";
+})
+E1.addEventListener("click", function () {
+    E.style.visibility="visible"
+    E.style.height= "auto";
+    C.style.visibility = "hidden";
+    C.style.height= "0px";
+    P.style.visibility = "hidden";
+    P.style.height= "0px";
+    B.style.visibility = "hidden";
+    B.style.height= "0px";
+})
+P1.addEventListener("click", function () {
+    P.style.visibility="visible"
+    P.style.height= "auto";
+    E.style.visibility = "hidden";
+    E.style.height= "0px";
+    C.style.visibility = "hidden";
+    C.style.height= "0px";
+    B.style.visibility = "hidden";
+    B.style.height= "0px";
+})
+B1.addEventListener("click", function () {
+    B.style.visibility="visible"
+    B.style.height= "auto";
+    E.style.visibility = "hidden";
+    E.style.height= "0px";
+    P.style.visibility = "hidden";
+    P.style.height= "0px";
+    C.style.visibility = "hidden";
+    C.style.height= "0px";
+})
