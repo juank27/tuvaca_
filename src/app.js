57e0786163115b75094cc296c37ea871e2d44d9a
juank27 <juankruiz27@gmail.com>
2022-07-19 19:36:52 -0500

    inicio de sesion con email

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 const express = require('express');//ayuda a crear un servidor
 const morgan = require('morgan'); //sirve para mostrar los request en consola
 const path = require('path');//permite trabajar con rutas
+const exphbs = require('express-handlebars');//permite trabajar con handlebars
 
 const app = express();
+
+app.set("views", path.join(__dirname, "views")); //permite trabajar con handlebars
+app.engine(
+	".hbs",
+	exphbs.create({
+		defaultLayout: false,//para poder usar layouts
+		extname: ".hbs",
+		//layoutsDir: "views/layouts/"
+	}).engine
+);
+app.set("view engine", ".hbs");
 
 app.use(morgan('dev'));
 app.use(express.json()); //para traer info del frontend con method POST
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 const { db,} = require('../firebase');//importar la base de datos
 const { dbFirebase, app, auth} = require('../firebaseCloud');//importar la base de datos
-const { createUserWithEmailAndPassword } = require('firebase/auth');
+const { createUserWithEmailAndPassword, signInWithEmailAndPassword } = require('firebase/auth');
 
 
 const router = Router();
@@ -18,37 +18,8 @@ router.get('/users', async (req, res) => {
 	console.log(users);
 	res.send('Hello World');
 });
-//crear un nuevo usuario
-router.post('/new-userr', async(req, res) => {
-	//crear un nuevo usuario
-	const { name, email, password, phone, ubication } = req.body;
-	await db.collection('users').add({
-		name,
-		email,
-		password,
-		phone,
-		ubication
-	})
-	res.send('new user');
-});
 */
-//crear un nuevo usuario
-function registrar(email, password) {
-	const auth = getAuth();
-	createUserWithEmailAndPassword(auth, email, password)
-		.then((userCredential) => {
-			// Signed in
-			const user = userCredential.user;
-			// ...
-		})
-		.catch((error) => {
-			const errorCode = error.code;
-			const errorMessage = error.message;
-			// ..
-		});
-}
 
-//clave nayis -> gKG0mb2v5qWE4CGYG5nUH5oLaBi2
 // new user email
 router.post('/new-user', async (req, res) => {
 	console.log('hola dentro de ');
@@ -67,7 +38,6 @@ router.post('/new-user', async (req, res) => {
 				// ...
 				const userRef = auth.currentUser;
 				//obteniendo el id del usuario userRef.uid
-				console.log(userRef.uid);
 				db.collection('users').doc(userRef.uid).set({
 					name,
 					email,
@@ -80,13 +50,108 @@ router.post('/new-user', async (req, res) => {
 				const errorCode = error.code;
 				const errorMessage = error.message;
 				// ..
-				console.log('error', error);
+				console.log('error', errorCode);
+				//res.sendStatus(errorCode).send(errorMessage);
 			});
 	}
 });
-//ba5Rv6P2s0PKHhhipvk8KilICEI3
-router.post('/new-userr', async (req, res) => {
-	console.log('hola dentro del segundo registro');
-	registrar(globalThis.email, globalThis.password);
+
+//login user email
+router.post('/login', async (req, res) => {
+	let { email, password } = req.body;
+	console.log(email, password);
+	signInWithEmailAndPassword(auth, email, password)
+		.then((userCredential) => {
+			// Signed in
+			const user = userCredential.user;
+			console.log('Login exitoso');
+			//console.log(user);
+			res.render('home');
+		})
+		.catch((error) => {
+			const errorCode = error.code;
+			const errorMessage = error.message;
+			console.log('error', errorCode);
+			//res.sendStatus(errorCode).send(errorMessage);
+		});
+	//res.send('login');
 });
+
+/*
+//nuevo usuario
+router.post('/new-user', async (req, res) => {
+	console.log('hola dentro de ');
+	var { password, confirmPassword, email, phone, ubication, name } = req.body;
+	console.log('--.-.-.-.-.-.-.-.-..-');
+	console.log(req.body);
+	var emaill = email;
+	var namee = name;
+	var phonee = phone;
+	var ubicationn = ubication;
+	var passwordd = password;
+	console.log(emaill);
+	console.log(namee);
+	console.log(phonee);
+	console.log(ubicationn);
+	if (password !== confirmPassword) {
+		let text = 'Las contraseñas no coinciden';
+		res.send(text);
+	} else {
+		User.registrar(emaill, passwordd, namee, emaill, phonee, ubicationn);
+		res.send('new user');
+	}
+});
+
+//Registro en BDD
+class User{
+	static registroBDD (name, email, phone, ubication, id) {
+		console.log('-.------------------------------------------------------');
+		console.log(name);
+		console.log(email);
+		console.log(phone);
+		console.log(ubication);
+		db.collection('users').doc(id).set({
+			name : name,
+			email : email,
+			phone : phone,
+			ubication : ubication,
+		})
+	}
+	//crear un nuevo usuario
+	static registrar(email, password, name, phone, ubication) {
+		console.log('dentro casiiiiiiiiii dentro  --------------------');
+		console.log(name);
+		console.log(email);
+		console.log(phone);
+		console.log(ubication);
+		//const auth = getAuth();
+		createUserWithEmailAndPassword(auth, email, password)
+			.then((userCredential) => {
+				// Signed in
+				const user = userCredential.user;
+				//console.log(user);
+				//console.log('registro exitoso');
+				// ...
+				const userRef = auth.currentUser;
+				console.log(userRef);
+				const id = userRef.uid;
+				//obteniendo el id del usuario userRef.uid
+				console.log('dentro de registrarn --------------------');
+				console.log(name);
+				console.log(email);
+				console.log(phone);
+				console.log(ubication);
+				User.registroBDD(name, email, phone, ubication, id);
+			})
+			.catch((error) => {
+				const errorCode = error.code;
+				const errorMessage = error.message;
+				// ..
+				console.log('error', error);
+			});
+	}
+}
+
+*/
+
 module.exports = router;
