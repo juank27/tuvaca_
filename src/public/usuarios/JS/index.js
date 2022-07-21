660966f529d75490e14bdbf03a80b4ca25712648
NayibeCasas <nayibepelaez03@gmail.com>
2022-07-21 14:30:19 -0500

    diseño de menu pagina de usuarios

--- src/public/usuarios/JS/index.js
+++ src/public/usuarios/JS/index.js
@@ -13,10 +13,7 @@ pub.addEventListener('click', function () {
     crear.style.transition = "0.2s";
     car.style.backgroundImage = 'url("../public/usuarios/icons/Truck.png")';
     car.style.transition = "0.2s";
-    chat.style.backgroundImage = 'url("../public/usuarios/icons/chat.png")';
-    chat.style.transition = "0.2s";
-    per.style.backgroundImage = 'url("../public/usuarios/icons/perfil.png")';
-    per.style.transition = "0.2s";
+
 })
 crear.addEventListener('click', function () {
     pub.style.backgroundImage = 'url("../public/usuarios/icons/Cow-1.png")';
@@ -25,10 +22,7 @@ crear.addEventListener('click', function () {
     crear.style.transition = "0.2s";
     car.style.backgroundImage = 'url("../public/usuarios/icons/Truck.png")';
     car.style.transition = "0.2s";
-    chat.style.backgroundImage = 'url("../public/usuarios/icons/chat.png")';
-    chat.style.transition = "0.2s";
-    per.style.backgroundImage = 'url("../public/usuarios/icons/perfil.png")';
-    per.style.transition = "0.2s";
+
 })
 car.addEventListener('click', function () {
     pub.style.backgroundImage = 'url("../public/usuarios/icons/Cow-1.png")';
@@ -37,23 +31,8 @@ car.addEventListener('click', function () {
     crear.style.transition = "0.2s";
     car.style.backgroundImage = 'url("../public/usuarios/icons/Truck2.png")';
     car.style.transition = "0.2s";
-    chat.style.backgroundImage = 'url("../public/usuarios/icons/chat.png")';
-    chat.style.transition = "0.2s";
-    per.style.backgroundImage = 'url("../public/usuarios/icons/perfil.png")';
-    per.style.transition = "0.2s";
 })
-chat.addEventListener('click', function () {
-    pub.style.backgroundImage = 'url("../public/usuarios/icons/Cow-1.png")';
-    pub.style.transition = "0.2s"
-    crear.style.backgroundImage = 'url("../public/usuarios/icons/Sum.png")';
-    crear.style.transition = "0.2s";
-    car.style.backgroundImage = 'url("../public/usuarios/icons/Truck.png")';
-    car.style.transition = "0.2s";
-    chat.style.backgroundImage = 'url("../public/usuarios/icons/chat1.png")';
-    chat.style.transition = "0.2s";
-    per.style.backgroundImage = 'url("../public/usuarios/icons/perfil.png")';
-    per.style.transition = "0.2s";
-})
+
 per.addEventListener('click', function () {
     pub.style.backgroundImage = 'url("../public/usuarios/icons/Cow-1.png")';
     pub.style.transition = "0.2s"
@@ -61,8 +40,4 @@ per.addEventListener('click', function () {
     crear.style.transition = "0.2s";
     car.style.backgroundImage = 'url("../public/usuarios/icons/Truck.png")';
     car.style.transition = "0.2s";
-    chat.style.backgroundImage = 'url("../public/usuarios/icons/chat.png")';
-    chat.style.transition = "0.2s";
-    per.style.backgroundImage = 'url("../public/usuarios/icons/perfil1.png")';
-    per.style.transition = "0.2s";
 })
