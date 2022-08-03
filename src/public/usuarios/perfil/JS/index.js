3387ba2d5d01f9717f8dcf1c3251078bfb242895
NayibeCasas <nayibepelaez03@gmail.com>
2022-08-02 21:24:51 -0500

    previsualizacion de imagenes

--- src/public/usuarios/perfil/JS/index.js
+++ src/public/usuarios/perfil/JS/index.js
@@ -5,21 +5,21 @@ let id;
 let cerrarSesion = document.querySelectorAll(".cerrar_modal_p")[0];
 let modalSesion = document.querySelectorAll(".modal_container_p")[0];
 let modalSesion2 = document.querySelectorAll(".modal_p")[0];
-let publicación=document.getElementById("publicacion");
-let id2=document.getElementById("id2");
-let framer=document.getElementById("framer");
+let publicación = document.getElementById("publicacion");
+let id2 = document.getElementById("id2");
+let framer = document.getElementById("framer");
 
-function posicion(valor){
+function posicion(valor) {
     // framer.contentDocument.location.reload(true);
     // id=document.querySelectorAll(".id")[valor].value;
     // id2.value=id;
     // publicación.submit();
-    setTimeout(function(){
+    setTimeout(function () {
         modal()
     }, 1000);
 }
-function modal(){
-    framer.src+= ''
+function modal() {
+    framer.src += ''
     modalSesion.style.opacity = "1";
     modalSesion.style.visibility = "visible";
     modalSesion2.classList.toggle("modal_cerrado_p");
@@ -29,7 +29,7 @@ cerrarSesion.addEventListener("click", function () {
     setTimeout(function () {
         modalSesion.style.opacity = "0";
         modalSesion.style.visibility = "hidden";
-        id.value="";
+        id.value = "";
     }, 900)
 })
 /************modal fotografias******/
@@ -87,3 +87,54 @@ cerrarSesionb.addEventListener("click", function () {
         modalSesion2c.style.visibility = "hidden";
     }, 900)
 })
+
+/************seleccionar fotos******/
+let elegir = document.querySelectorAll(".insert_Foto")[0];
+let imagen = document.querySelectorAll(".imagen")[0];
+let filesI = document.getElementById("foto_publicacion");
+let filesI2 = document.getElementById("foto_publicacion2");
+
+elegir.addEventListener("click", function () {
+    filesI.click();
+})
+let contador = 0;
+let contador2 = 0;
+var element2 = [];
+filesI.addEventListener("change", function () {
+    console.log(this.files);
+    var files = this.files;
+    var element;
+    for (var i = 0; i < files.length; i++) {
+        element = files[i];
+        element2[contador2] = element;
+        contador2++
+
+        if (files.length > 5 || contador === 5) {
+        } else {
+            createPreview(element);
+            contador = contador + 1;
+            filesI.disabled = false;
+        }
+    }
+    if (files.length > 5 || contador === 5) {
+        alert("solo puede subir 5 imagenes")
+    }
+    console.log("Archivo N")
+    for (var j = 0; j < element2.length; j++) {
+        // console.log(element2[j])
+    }
+    filesI2=element2;
+    console.log(filesI2)
+
+})
+
+function createPreview(file) {
+    var imgCodified = URL.createObjectURL(file);
+    var img = $('<div class="cont_img"> <img src="' + imgCodified + '" class="imagen""><div class="cerrar"><div class="cerrar2"></div></div></div>');
+    $(img).insertBefore(".insert_Foto");
+}
+$(document).on("click", ".cont_img", function (e) {
+    $(this).remove();
+    contador = contador - 1;
+
+});
