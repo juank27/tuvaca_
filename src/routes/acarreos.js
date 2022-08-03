c11eca2a1331871a4277c2dde3fc18888c5adcf5
juank27 <juankruiz27@gmail.com>
2022-08-02 23:48:06 -0500

    envio de imagen [0], a firebase, se debe crear bucle para enviar todas , y poder enviarla con el tipo correspondiente

--- src/routes/acarreos.js
+++ src/routes/acarreos.js
@@ -1,20 +1,38 @@
 const { Router } = require('express');
 const multer = require('multer')
+const { storage } = require('../firebaseCloud');//importar la base de datos
+const { getStorage, ref, uploadBytes } = require ("firebase/storage");
+
+
 
 
 const router = Router();
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+//const storageCloud = getStorage();
+const storageLocal = multer.memoryStorage();
+const upload = multer({ storage: storageLocal });
+
 
 router.get('/imga', (req, res) => {
 	res.render('img', {layout: false});
 });
 
-router.post('/imagen', upload.array('imagen[]', 3),  function (req, res) {
+router.post('/imagen', upload.array('imagen[]', 3),  async (req, res) =>{
 	console.log(req.files);
 	console.log('--------------');
-	console.log(req.files[1]);
-	res.send('enviado');
+	console.log(typeof(req.files[0]));
+	let files = req.files;
+	const storageRef = ref(storage, 'images/'+files[0].originalname);
+	console.log('holaaa');
+	console.log(storageRef);
+	uploadBytes(storageRef, req.files[0].buffer)
+		.then((snapshot) => {
+			console.log('Uploaded a blob or file!');
+			res.send('enviado');
+		})
+		.catch((error) => {
+			console.log(error);
+			res.send('error');
+		});
 });
 
 module.exports = router;
