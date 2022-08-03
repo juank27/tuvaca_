4a409da23e26c7dd63af4adc0ff87390a6b3325a
juank27 <juankruiz27@gmail.com>
2022-08-03 00:58:25 -0500

    guardando publicaciones con funcion prueba, esta bien

--- src/routes/acarreos.js
+++ src/routes/acarreos.js
@@ -1,10 +1,8 @@
 const { Router } = require('express');
 const multer = require('multer')
 const { storage } = require('../firebaseCloud');//importar la base de datos
-const { getStorage, ref, uploadBytes } = require ("firebase/storage");
-
-
-
+const { ref, uploadBytes, getDownloadURL } = require ("firebase/storage");
+const { db, } = require('../firebase');//importar la base de datos
 
 const router = Router();
 //const storageCloud = getStorage();
@@ -16,8 +14,45 @@ router.get('/imga', (req, res) => {
 	res.render('img', {layout: false});
 });
 
+// envio de imagenes al servidor en firebase storage
 router.post('/imagen', upload.array('imagen[]', 3),  async (req, res) =>{
 	let files = req.files;
+	// for (let index = 0; index < files.length; index++) {
+	// 	const storageRef = ref(
+	// 		storage,
+	// 		'images/' + new Date().getTime() + files[index].originalname
+	// 	);
+	// 	const metadata = {
+	// 		contentType: files[index].mimetype,
+	// 	};
+	// 	uploadBytes(storageRef, req.files[index].buffer, metadata)
+	// 		.then((snapshot) => {
+	// 			console.log('Uploaded a blob or file!');
+	// 			console.log(snapshot.metadata.fullPath);
+	// 			getDownloadURL(ref(storage, snapshot.metadata.fullPath))
+	// 				.then((url) => {
+	// 					console.log(url);
+	// 				})
+	// 				.catch((error) => {
+	// 					console.log(error);
+	// 				});
+	// 		})
+	// 		.catch((error) => {
+	// 			console.log(error);
+	// 			res.send('error');
+	// 		});
+	// }
+	// sendImages(files, 1)
+	// 	.then((links) => {
+	// 		console.log('dentro de la funcioooon');
+	// 		console.log(links);
+	// 	});
+	//sendImages(files, enviar);
+	sendImages(files, enviar);
+	res.send('enviado');
+});
+
+function sendImages(files, callback) {
 	for (let index = 0; index < files.length; index++) {
 		const storageRef = ref(
 			storage,
@@ -26,16 +61,29 @@ router.post('/imagen', upload.array('imagen[]', 3),  async (req, res) =>{
 		const metadata = {
 			contentType: files[index].mimetype,
 		};
-		uploadBytes(storageRef, req.files[index].buffer, metadata)
+		uploadBytes(storageRef, files[index].buffer, metadata)
 			.then((snapshot) => {
 				console.log('Uploaded a blob or file!');
+				console.log(snapshot.metadata.fullPath);
+				getDownloadURL(ref(storage, snapshot.metadata.fullPath))
+					.then((url) => {
+						console.log(url);
+						callback(url);
+					})
+					.catch((error) => {
+						console.log(error);
+					});
 			})
 			.catch((error) => {
 				console.log(error);
-				res.send('error');
 			});
 	}
-		res.send('enviado');
-});
-
+}
+//funcion para guardar publicaciones
+function enviar(url) {
+	db.collection('nuevo').doc().set({
+		hola: 'hola',
+		url,
+	});
+}
 module.exports = router;
