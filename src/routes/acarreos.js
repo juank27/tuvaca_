eb04d947171d821176aa65757b9aa8b4667cf4ca
juank27 <juankruiz27@gmail.com>
2022-08-05 15:09:28 -0500

    subida de imagenes multiple, con unico input, y ayuda de la nueva funcion multer FILES

--- src/routes/acarreos.js
+++ src/routes/acarreos.js
@@ -38,9 +38,60 @@ router.post('/imagenPrueba', (req, res) => {
 		});
 	//---------------------------------
 });
-
+//imagen prueba con seleccion de imagenes con multer
+let multpleInput = upload.fields([
+	{ name: 'input0' },
+	{ name: 'input1' },
+	{ name: 'input2' },
+	{ name: 'input3' },
+	{ name: 'input4' }
+]);
+// envio de imagenes al servidor en firebase storage
+router.post('/imagen', upload.array('imagen[]', 3),  async (req, res) =>{
+	let files = req.files;
+	sendImages(files, enviar);
+	res.send('enviado');
+});
+router.post('/multiple_input', multpleInput, (req, res) => {
+	let files = req.files;
+	//let inputt = "input"+1;
+	// console.log(files[inputt][0].originalname);
+	// console.log(Object.keys(files).length);
+	sendImages(files, enviar);
+	res.send('recibidos');
+});
 // funciones para guardar imagenes en firebase storage
 function sendImages(files, callback) {
+	console.log(files);
+	for (let index = 0; index < Object.keys(files).length; index++) {
+		let inputt = "input" + index;
+		const storageRef = ref(
+			storage,
+			'images/' + new Date().getTime() + files[inputt][0].originalname
+		);
+		const metadata = {
+			contentType: files[inputt][0].mimetype,
+		};
+		uploadBytes(storageRef, files[inputt][0].buffer, metadata)
+			.then((snapshot) => {
+				console.log('Uploaded a blob or file!');
+				console.log(snapshot.metadata.fullPath);
+				getDownloadURL(ref(storage, snapshot.metadata.fullPath))
+					.then((url) => {
+						console.log(url);
+						callback(url);
+					})
+					.catch((error) => {
+						console.log(error);
+					});
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}
+}
+// funciones para guardar imagenes en firebase storage
+function sendImagess(files, callback) {
 	for (let index = 0; index < files.length; index++) {
 		const storageRef = ref(
 			storage,
