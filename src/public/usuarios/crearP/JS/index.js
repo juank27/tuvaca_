34f4565ff2263f7f781457724fafd433380c60e0
NayibeCasas <nayibepelaez03@gmail.com>
2022-08-10 18:48:57 -0500

    actualizacion con merge a rama juank27, activacion de required en crear publicaciones dependiento la accion que elija, campo de previsulizacion se activa segun video e imagen

--- src/public/usuarios/crearP/JS/index.js
+++ src/public/usuarios/crearP/JS/index.js
@@ -8,15 +8,21 @@ let nprenada = document.getElementById("nprenada");
 categoria.addEventListener("click", function () {
     if (categoria.value === "Vaca" || categoria.value === "Novilla") {
         prenada.style.display = "flex";
-        prenada.required="true";
+        prenada.required = true;
     } else {
         prenada.style.display = "none";
-        prenada.requires="false";
+        prenada.requires = false;
         tipoprenada.style.display = "none";
         toro.style.display = "none";
         semanas.style.display = "none";
         primeriza.style.display = "none";
         nprenada.style.display = "none";
+        tipoprenada.required = false;
+        toro.required = false;
+        semanas.required = false;
+        primeriza.required = false;
+        nprenada.required = false;
+
     }
 })
 prenada.addEventListener("click", function () {
@@ -25,28 +31,29 @@ prenada.addEventListener("click", function () {
         toro.style.display = "flex";
         semanas.style.display = "flex";
         primeriza.style.display = "flex";
-        tipoprenada.required="true";
-        toro.required="true";
-        semanas.required="true";
-        primeriza.required="true";
-    } else {
+        tipoprenada.required = true;
+        toro.required = true;
+        semanas.required = true;
+        primeriza.required = true;
+    } if (prenada.value === "No esta preñada" || prenada.value === "") {
         tipoprenada.style.display = "none";
         toro.style.display = "none";
         semanas.style.display = "none";
         primeriza.style.display = "none";
-        tipoprenada.required="false";
-        toro.required="false";
-        semanas.required="false";
-        primeriza.required="false";
+        tipoprenada.required = false;
+        toro.required = false;
+        semanas.required = false;
+        primeriza.required = false;
+        nprenada.required = false;
     }
 })
 primeriza.addEventListener("click", function () {
     if (primeriza.value === "No es primeriza") {
         nprenada.style.display = "flex";
-        nprenada.required="true";
-    } else {
+        nprenada.required = true;
+    } if (primeriza.value === "Si es primeriza" || primeriza.value === "") {
         nprenada.style.display = "none";
-        nprenada.required="false";
+        nprenada.required = false;
     }
 })
 /************seleccionar fotos******/
@@ -70,31 +77,35 @@ let IF_2 = document.querySelectorAll(".IF_2")[0];
 let f2 = document.querySelectorAll(".f2")[0];
 let c_2 = document.querySelectorAll(".c_2")[0];
 let ct_2 = document.querySelectorAll(".ct_2")[0];
-let im_2 = document.querySelectorAll(".im_2")[0];
+let im2 = document.querySelectorAll(".im_2")[0];
+let vd2 = document.querySelectorAll(".vd_2")[0];
 /*******input 3***** */
 let IF_3 = document.querySelectorAll(".IF_3")[0];
 let f3 = document.querySelectorAll(".f3")[0];
 let c_3 = document.querySelectorAll(".c_3")[0];
 let ct_3 = document.querySelectorAll(".ct_3")[0];
-let im_3 = document.querySelectorAll(".im_3")[0];
+let im3 = document.querySelectorAll(".im_3")[0];
+let vd3 = document.querySelectorAll(".vd_3")[0];
 /*******input 4***** */
 let IF_4 = document.querySelectorAll(".IF_4")[0];
 let f4 = document.querySelectorAll(".f4")[0];
 let c_4 = document.querySelectorAll(".c_4")[0];
 let ct_4 = document.querySelectorAll(".ct_4")[0];
-let im_4 = document.querySelectorAll(".im_4")[0];
-let IF_5 = document.querySelectorAll(".IF_5")[0];
+let im4 = document.querySelectorAll(".im_4")[0];
+let vd4 = document.querySelectorAll(".vd_4")[0];
 /*******input 5***** */
+let IF_5 = document.querySelectorAll(".IF_5")[0];
 let f5 = document.querySelectorAll(".f5")[0];
 let c_5 = document.querySelectorAll(".c_5")[0];
 let ct_5 = document.querySelectorAll(".ct_5")[0];
-let im_5 = document.querySelectorAll(".im_5")[0];
+let im5 = document.querySelectorAll(".im_5")[0];
+let vd5 = document.querySelectorAll(".vd_5")[0];
 let limite = document.querySelectorAll(".limite")[0];
 
 /*******posicion***** */
 let array1 = [1, 2, 3, 4];
 let array2 = [0, 0, 0, 0];
-let valor=0;
+let valor = 0;
 
 /****input 1******/
 IF_1.addEventListener("click", function () {
@@ -102,7 +113,7 @@ IF_1.addEventListener("click", function () {
 })
 f1.addEventListener("change", function () {
     var files = this.files;
-    valor=files.length;
+    valor = files.length;
     console.log(valor);
     visualizar2(files[0]);
 })
@@ -110,11 +121,11 @@ f1.addEventListener("change", function () {
 /****agregar imagenes*****/
 mas.addEventListener("click", function () {
     let posicion;
-    limite.style.color="red";
-    for(var i=0;i<=4;i++){
-        if(array1[i]===(i+1)){
-            posicion=array1[i];
-            if(i===3){
+    limite.style.color = "red";
+    for (var i = 0; i <= 4; i++) {
+        if (array1[i] === (i + 1)) {
+            posicion = array1[i];
+            if (i === 3) {
             }
             break;
         }
@@ -150,7 +161,7 @@ f2.addEventListener("change", function () {
     var files = this.files;
     array2[0] = 1;
     array1[0] = 0;
-    visualizar(files[0], im_2, IF_2, ct_2);
+    visualizar(files[0], IF_2, ct_2, im2,vd2);
 })
 c_2.addEventListener("click", function () {
     IF_2.style.display = "none";
@@ -168,7 +179,7 @@ f3.addEventListener("change", function () {
     var files = this.files;
     array2[1] = 2;
     array1[1] = 0;
-    visualizar(files[0], im_3, IF_3, ct_3);
+    visualizar(files[0], IF_3, ct_3, im3,vd3);
 })
 c_3.addEventListener("click", function () {
     IF_3.style.display = "none";
@@ -186,7 +197,7 @@ f4.addEventListener("change", function () {
     var files = this.files;
     array2[2] = 3;
     array1[2] = 0;
-    visualizar(files[0], im_4, IF_4, ct_4);
+    visualizar(files[0], IF_4, ct_4, im4,vd4);
 })
 c_4.addEventListener("click", function () {
     IF_4.style.display = "none";
@@ -204,7 +215,7 @@ f5.addEventListener("change", function () {
     var files = this.files;
     array2[3] = 4;
     array1[3] = 0;
-    visualizar(files[0], im_5, IF_5, ct_5);
+    visualizar(files[0], IF_5, ct_5, im5,vd5);
 })
 c_5.addEventListener("click", function () {
     IF_5.style.display = "none";
@@ -217,34 +228,45 @@ c_5.addEventListener("click", function () {
 
 
 /*******funciones*******/
-function visualizar(file, imagen, input, foto) {
+function visualizar(file, input, foto, imagen,video) {
     var imgCodified = URL.createObjectURL(file);
-    imagen.src = imgCodified;
-    foto.style.display = "flex";
-    input.style.display = "none";
+
+    if (file.type === "video/mp4") {
+        video.style.display="flex";
+        video.src=imgCodified;
+        foto.style.display = "flex";
+        input.style.display = "none";
+        imagen.style.display="none";
+    } else {
+        imagen.style.display="flex";
+        imagen.src=imgCodified;
+        foto.style.display = "flex";
+        input.style.display = "none";
+        video.style.display="none";
+    }
 }
 function visualizar2(file, imagen2) {
     var imgCodified = URL.createObjectURL(file);
-    separador.style.display="none";
-    requerido.style.display="none";
-    IF_1.style.display="none";
-    foto.style.display="flex";
+    separador.style.display = "none";
+    requerido.style.display = "none";
+    IF_1.style.display = "none";
+    foto.style.display = "flex";
     principal.src = imgCodified;
 
 }
 foto.addEventListener("click", function () {
-    foto.style.display="none";
-    separador.style.display="flex";
-    requerido.style.display="flex";
-    IF_1.style.display="flex";
-    principal.src="./usuarios/menu/icons/camera.png";
+    foto.style.display = "none";
+    separador.style.display = "flex";
+    requerido.style.display = "flex";
+    IF_1.style.display = "flex";
+    principal.src = "./usuarios/menu/icons/camera.png";
 
 })
 subir.addEventListener("click", function () {
-    if(valor===0){
+    if (valor === 0) {
         modalf.style.display = "flex";
-        requerido.style.color="red";
-        requerido.innerHTML="Debe elegir una imagen de portada";
-        IF_1.style.border="5px dashed red";
+        requerido.style.color = "red";
+        requerido.innerHTML = "Debe elegir una imagen de portada";
+        IF_1.style.border = "5px dashed red";
     }
 })
